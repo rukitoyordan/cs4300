5d644e464d7e544122bdc3211e2c47e741f8e95f
Fabian Perez Munoz <fperezmu@uccs.edu>
2026-09-13 19:19:43 +0000

    implemented task2 and pytests (parametrized)

---
+++ homework1/src/task2.py
@@ -0,0 +1,16 @@
+# Integers
+def integer_mult(int1, int2):
+    return int1 * int2
+
+# Floating-Point Numbers
+def float_division(float1, float2):
+    return float1/float2
+
+# String
+def string_invert(str):
+    string_reversed = str[::-1]
+    return string_reversed
+
+# Boolean
+def is_capitalized(str):
+    return str.isupper()
---
+++ homework1/tests/test_task2.py
@@ -0,0 +1,32 @@
+import pytest
+# src to be able to run globally from homework1/
+from src import task2
+
+
+# Tests: Integer
+@pytest.mark.parametrize("int1, int2, expected", [(8, 7, 56), (-6, -6, 36), (-4, 3, -12)])
+def test_integer_mult(int1, int2, expected):
+    assert task2.integer_mult(int1, int2) == expected
+
+# Tests: Floating-Point Numbers
+@pytest.mark.parametrize("float1, float2, expected", [(10.0, 5.0, 2.0), (29.7, -4.8, -6.1875), (7.0, 16.0, 0.4375)])
+def test_integer_mult(float1, float2, expected):
+    assert task2.float_division(float1, float2) == expected
+
+# Tests: Strings
+@pytest.mark.parametrize("str, expected", [("friends", "sdneirf"), ("five hundred twenty", "ytnewt derdnuh evif"), (" ", " ")])
+def test_integer_mult(str, expected):
+    assert task2.string_invert(str) == expected
+
+# Test: Booleans
+@pytest.mark.parametrize("str, expected", [("HASTINGS", True), ("hastings", False), ("", False)])
+def test_integer_mult(str, expected):
+    assert task2.is_capitalized(str) == expected
+
+# https://docs.pytest.org/en/stable/how-to/parametrize.html
+
+'''
+@pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
+def test_eval(test_input, expected):
+    assert eval(test_input) == expected
+'''
