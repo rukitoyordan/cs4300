dcaf8d9c5c6c15a0b957b3a8a500be3811a19c86
Fabian Perez Munoz <fperezmu@uccs.edu>
2026-09-14 02:26:45 +0000

    implemented task5 and pytests

---
+++ homework1/src/task5.py
@@ -0,0 +1,15 @@
+def list_favorite_books():
+    '''Create a list of your favorite books, including book titles and authors.'''
+    favorite_books = ["The Rise of Kyoshi by F.C. Yee", "Beautiful Creatures by Kami Garcia and Margaret Stohl", "The Four Agreements by Don Miguel Ruiz", "Origin by Dan Brown"]
+    return favorite_books
+
+def obtain_first_three_books(favorite_books):
+    '''Apply list sclicing to print the first three books in the favorite books list.'''
+    first_three_books = favorite_books[:3]
+    print(first_three_books)
+    return first_three_books
+
+def basic_student_database():
+    '''Represents a basic student database, including student names and their corresponding student IDs.'''
+    student_database = {"Edgar Feliciano": 802214567, "Ruth Shepard": 802229874, "Jason Laboy": 802193523, "John Stevenson": 802164211}
+    return student_database
---
+++ homework1/tests/test_task5.py
@@ -0,0 +1,34 @@
+import pytest
+# src to be able to run globally from homework1/
+from src import task5
+
+
+# Tests: Favorite Books
+def test_list_favorite_books():
+    favorite_books = task5.list_favorite_books()
+    assert type(favorite_books) is list
+    assert len(favorite_books) == 4
+    assert "The Four Agreements by Don Miguel Ruiz" in favorite_books
+    assert "The Rise of Kyoshi by F.C. Yee" in favorite_books
+    assert "Origin by Dan Brown" in favorite_books
+    assert "Beautiful Creatures by Kami Garcia and Margaret Stohl" in favorite_books
+
+def test_obtain_first_three_books():
+    favorite_books = task5.list_favorite_books()
+    first_three_books = task5.obtain_first_three_books(favorite_books)
+    assert type(first_three_books) is list
+    assert len(first_three_books) == 3
+    # Check if the slicing works properly.
+    assert first_three_books == favorite_books[:3] 
+    # Ensures the fourht book is not within the three books.
+    assert "Origin by Dan Brown" not in first_three_books
+
+def test_basic_student_database():
+    student_database = task5.basic_student_database()
+    assert type(student_database) is dict
+    assert len(student_database) == 4
+    proposed_database = {"Edgar Feliciano": 802214567, 
+        "Ruth Shepard": 802229874, 
+        "Jason Laboy": 802193523, 
+        "John Stevenson": 802164211}
+    assert student_database == proposed_database
