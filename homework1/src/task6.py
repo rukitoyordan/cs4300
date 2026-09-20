c1d68d1d02469c092f1a7a5a25b83a2974d9593f
Fabian Perez Munoz <fperezmu@uccs.edu>
2026-09-20 21:38:58 +0000

    fix naming convention and add encoding for readability

--- homework1/src/task6.py
+++ homework1/src/task6.py
@@ -2,19 +2,19 @@ import os
 # https://docs.python.org/3/library/os.html
 # https://www.geeksforgeeks.org/python/python-get-number-of-characters-words-spaces-and-lines-in-a-file/
 
-file = "task6_read_me.txt"
+file_name = "task6_read_me.txt"
 
-def word_counter(file):
+def word_counter(file_name):
     '''Reads task6_read_me.txt and counts the number of words in it.'''
-    words = 0
+    word_count = 0
     # Check if the file exists properly or not.
-    if os.path.exists(file) == False:
+    if not os.path.exists(file_name):
         return 0
 
-    with open(file, "r") as count_file:
-        for x in count_file:
-            word = x.split()
-            words += len(word)
+    with open(file_name, "r", encoding="utf-8") as count_file:
+        for line in count_file:
+            word = line.split()
+            word_count += len(word)
 
-    return words
+    return word_count
 
