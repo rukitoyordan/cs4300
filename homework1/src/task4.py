8449fc027d25b9a36a155976ebbf42e350a5ee13
Fabian Perez Munoz <fperezmu@uccs.edu>
2026-09-20 20:40:09 +0000

    update task4 to include better validation testing and acceptable ranges for price/discount

--- homework1/src/task4.py
+++ homework1/src/task4.py
@@ -1,6 +1,12 @@
 def calculate_discount(price, discount):
     """Calculates the final price of a product after applying a given discount percentage."""
+    try:
+        if price < 0 or discount < 0 or discount > 100:
+            raise ValueError("Please provide valid pricing and discount percentage amounts.")
     discounted_price = price*(discount/100)
     return (price - discounted_price)
+    except TypeError as numeric_error:
+        raise TypeError("Please provide a numeric pricing and discount percentage, rather than a string.") from numeric_error
+
 
 # "If it looks like a duck and quacks like a duck, it's a duck"
--- homework1/tests/test_task4.py
+++ homework1/tests/test_task4.py
@@ -8,7 +8,21 @@ from src import task4
 # Tests: Final Pricing after Discount
 @pytest.mark.parametrize("price, discount, expected", [
     # Some edge cases were discovered/learned about by using AI.
-    (100, 20, 80),(100, 100, 0),(True, 15, 0.85),(3.899, 10, 3.5091),(0.0, 50, 0.0)
+    (100, 20, 80),(100, 100, 0),(200, 12.5, 175.0),(80.0, 12.5, 70.0),(True, 15, 0.85),(3.899, 10, 3.5091),(0.0, 50, 0.0)
 ])
 def test_calculate_discount(price, discount, expected):
     assert task4.calculate_discount(price, discount) == expected
+
+@pytest.mark.parametrize("price, discount", [
+    ("fifty dollars", 25),(49.99, "ten percent"),(None, 75),("100", 20.5),(100, "20")
+])
+def test_calculate_discount_numbers_only_check(price, discount):
+    with pytest.raises(TypeError):
+        task4.calculate_discount(price, discount)
+
+@pytest.mark.parametrize("price, discount", [
+    (-1, 25),(65, -4),(100, 101),(-30, 25)
+])
+def test_calculate_discount_range_acceptable(price, discount):
+    with pytest.raises(ValueError):
+        task4.calculate_discount(price, discount)
