eef0ee796735b1eb227bae08a19f76777641aa42
Fabian Perez Munoz <fperezmu@uccs.edu>
2026-09-20 21:44:08 +0000

    fix syntax and naming convention in task3

--- homework1/src/task3.py
+++ homework1/src/task3.py
@@ -2,9 +2,9 @@ from sympy import isprime
 
 def check_number_status(number):
     """Checks if a given number is either positive, negative or zero. It returns a statement declaring what type the number is."""
-    if(number) > 0:
+    if number > 0:
         return "this number is positive."
-    elif(number) < 0:
+    elif number < 0:
         return "this number is negative."
     else:
         return "this number is zero."
@@ -14,7 +14,7 @@ def first_ten_prime_numbers():
     """Loops to print the first 10 prime numbers."""
     prime_numbers = []
 
-    for number in range(2,2000):
+    for number in range(2, 2000):
         if len(prime_numbers) == 10:
             break
         if isprime(number):
@@ -24,10 +24,10 @@ def first_ten_prime_numbers():
 
 def sum_all_one_to_hundred():
     '''Finds the sum of all numbers from 1 to 100. Sum is 5050'''
-    sum = 0
+    sum_total = 0
     count = 0
 
     while count <= 100:
-        sum += count
+        sum_total += count
         count += 1
-    return sum
+    return sum_total
